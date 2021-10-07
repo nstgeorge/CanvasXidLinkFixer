62a73af56b97eab5d4f2f7f602b1a6b31555c249
Nate St. George <natestgeorge@gmail.com>
2021-10-07 12:30:20 -0600

    Add support for multiple courses per run and ID only; bug fixes

--- main.py
+++ main.py
@@ -121,6 +121,11 @@ def exists_css_selector(driver, selector):
     return True
 
 
+def get_course_link(id):
+    """Returns the course link for a given course ID."""
+    return BASE_URL + "/courses/" + id
+
+
 def open_in_new_tab(driver, link):
     """Opens the given link in a new tab."""
     ActionChains(driver).key_down(Keys.CONTROL).click(link).key_up(Keys.CONTROL).perform()
@@ -252,17 +257,19 @@ def handle_quiz_question(driver):
 
 def handle_page(driver):
     """Handle a page with an xid link."""
-    ui.WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_class_name("edit-wiki")).click()
-    tinymce = ui.WebDriverWait(driver, 10).until(
-        lambda driver: driver.find_element_by_class_name("tox-edit-area__iframe")
-    )
-    replace_xid_in_tinymce(driver, tinymce)
-    driver.find_element_by_css_selector("button[class*=submit]").click()
+    try:
+        ui.WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_class_name("edit-wiki")).click()
+        tinymce = ui.WebDriverWait(driver, 10).until(
+            lambda driver: driver.find_element_by_class_name("tox-edit-area__iframe")
+        )
+        replace_xid_in_tinymce(driver, tinymce)
+        driver.find_element_by_css_selector("button[class*=submit]").click()
+    except Exception:
+        print("Unable to fix this page.")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Fix broken \"xid\" links in a Canvas course.")
-    parser.add_argument('course', help="Link to the Canvas course to modify. Expects a direct homepage link.")
     parser.add_argument('--force-refresh-links',
                         dest="refresh",
                         default=False,
@@ -274,76 +281,81 @@ if __name__ == "__main__":
                         default=False,
                         action="store_true",
                         help="If specified, loads ChromeDriver instead of geckodriver.")
+    parser.add_argument('-c', '--course', nargs="+", required=True,
+                        help="Link(s) to the Canvas course(s) to modify.")
 
     args = parser.parse_args()
     browser = webdriver.Firefox() if not args.chrome else webdriver.Chrome()
 
     with contextlib.closing(browser) as driver:
-        print("Beginning course link fix on {}...".format(args.course))
-        driver.get(args.course)
-        if "Log In" in driver.title:
-            print("Please log into your Boise State account to access this course. In {} seconds, this request will "
-                  "time out.".format(LOGIN_TIMEOUT))
-            wait = ui.WebDriverWait(driver, LOGIN_TIMEOUT)
-        go_to_course_link_validator(driver, wait)
+        for course in args.course:
+            print("Beginning course link fix on {}...".format(course))
+            driver.get(get_course_link(course) if course.isnumeric() else course)
 
-        # Find results
-        results = driver.find_elements_by_class_name("result")
-        if len(results) == 0 or args.refresh:
-            print("Refreshing broken links. This request will time out in 10 minutes.")
-            wait_links = ui.WebDriverWait(driver, REFRESH_TIMEOUT)
-            try:
-                driver.find_element_by_partial_link_text("Link Validation").click()
-            except NoSuchElementException:
-                print("Unable to refresh link validation: element not found")
+            if "Log In" in driver.title:
+                print("Please log into your Boise State account to access this course. In {} seconds, this request will "
+                      "time out.".format(LOGIN_TIMEOUT))
+                wait = ui.WebDriverWait(driver, LOGIN_TIMEOUT)
+            go_to_course_link_validator(driver, wait)
 
-            results = wait_links.until(lambda driver: driver.find_elements_by_class_name("result"))
+            # Find results
+            results = driver.find_elements_by_class_name("result")
+            if len(results) == 0 or args.refresh:
+                print("Refreshing broken links. This request will time out in 10 minutes.")
+                wait_links = ui.WebDriverWait(driver, REFRESH_TIMEOUT)
+                try:
+                    driver.find_element_by_partial_link_text("Link Validation").click()
+                except NoSuchElementException:
+                    print("Unable to refresh link validation: element not found")
 
-        # Filter results to only those with "xid" in the link
-        xid_items = [r for r in results if len(r.find_elements_by_partial_link_text("xid")) != 0]
+                results = wait_links.until(lambda driver: driver.find_elements_by_class_name("result"))
 
-        print("{} xid items found. Beginning fixes...".format(len(xid_items)))
+            # Filter results to only those with "xid" in the link
+            xid_items = [r for r in results if len(r.find_elements_by_partial_link_text("xid")) != 0]
 
-        main_window = driver.current_window_handle
-        fixed_banks = []
+            print("{} xid items found. Beginning fixes...".format(len(xid_items)))
 
-        # Detect item type and send to proper function.
-        # Normally I would navigate to the correct element and do a switch on the text,
-        # but Canvas uses non-human-readable class names for these elements and I don't want the system to break
-        # if the class names are nondeterministic, which seems likely.
-        for item in xid_items:
-            handle_count = len(driver.window_handles)
+            main_window = driver.current_window_handle
+            fixed_banks = []
 
-            url = item.find_element_by_tag_name("h2").find_element_by_tag_name("a").get_attribute("href")
-            url = url.split("#")[0]
-            if url in fixed_banks:
-                print("This question has already been fixed because it belongs to the same bank as a previous question.")
-                continue
+            # Detect item type and send to proper function.
+            # Normally I would navigate to the correct element and do a switch on the text,
+            # but Canvas uses non-human-readable class names for these elements and I don't want the system to break
+            # if the class names are nondeterministic, which seems likely.
+            for item in xid_items:
+                handle_count = len(driver.window_handles)
 
-            print(url)
-            fixed_banks.append(url)
+                url = item.find_element_by_tag_name("h2").find_element_by_tag_name("a").get_attribute("href")
+                url = url.split("#")[0]
+                if url in fixed_banks:
+                    print("This question has already been fixed because it belongs to the same bank as a previous question.")
+                    continue
 
-            driver.execute_script("window.open('{}', '_blank')".format(url))
+                print(url)
+                fixed_banks.append(url)
 
-            # Wait until the new tab is open
-            wait = ui.WebDriverWait(driver, 10)
-            wait.until(lambda driver: len(driver.window_handles) != handle_count)
+                driver.execute_script("window.open('{}', '_blank')".format(url))
 
-            # Handle different types of pages
-            if len(find_elements_by_text(item, "Assessment Question")) != 0:
-                # print(len(find_elements_by_text(item, "Assessment Question")))
-                driver.switch_to.window(driver.window_handles[1])
-                handle_assessment_question_pool(driver)
-            elif len(find_elements_by_text(item, "Quiz Question")) != 0:
-                driver.switch_to.window(driver.window_handles[1])
-                handle_quiz_question(driver)
-            elif len(find_elements_by_text(item, "Page")) != 0 or len(find_elements_by_text(item, "Assignment")) != 0:
-                driver.switch_to.window(driver.window_handles[1])
-                handle_page(driver)
-            else:
-                pass
+                # Wait until the new tab is open
+                wait = ui.WebDriverWait(driver, 10)
+                wait.until(lambda driver: len(driver.window_handles) != handle_count)
 
-            driver.close()
-            driver.switch_to.window(main_window)
+                # Handle different types of pages
+                if len(find_elements_by_text(item, "Assessment Question")) != 0:
+                    # print(len(find_elements_by_text(item, "Assessment Question")))
+                    driver.switch_to.window(driver.window_handles[1])
+                    handle_assessment_question_pool(driver)
+                elif len(find_elements_by_text(item, "Quiz Question")) != 0:
+                    driver.switch_to.window(driver.window_handles[1])
+                    handle_quiz_question(driver)
+                elif len(find_elements_by_text(item, "Page")) != 0 or len(find_elements_by_text(item, "Assignment")) != 0:
+                    driver.switch_to.window(driver.window_handles[1])
+                    handle_page(driver)
+                else:
+                    pass
+
+                driver.close()
+                driver.switch_to.window(main_window)
+            print("Course complete.")
 
         print("Done.")
