ace74e4e24673a405d37cd9f2e33bcf66a5f14df
Nate St. George <natestgeorge@gmail.com>
2021-10-19 16:58:03 -0600

    Fix error alert

--- start.py
+++ start.py
@@ -65,7 +65,7 @@ def run_fix():
                     alert.error("Failed to log into your Boise State account. Please log out "
                                 "and re-enter your information.")
                 if err == "login_not_interactable":
-                    alert.err("Unable to interact with login page. This is usually fixed with a rerun.")
+                    alert.error("Unable to interact with login page. This is usually fixed with a rerun.")
                 else:
                     alert.error("An unknown error occurred. Code: {}. Stopping.".format(err))
                 break
