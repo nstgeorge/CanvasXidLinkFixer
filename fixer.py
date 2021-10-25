3a6244b5e1e1334f50464092011d503779164f83
Nate St. George <natestgeorge@gmail.com>
2021-10-25 15:06:47 -0600

    Attempt fix to premature login fail error

--- fixer.py
+++ fixer.py
@@ -176,6 +176,9 @@ class XIDFixer:
         return (element if element else self.__driver).find_elements(By.XPATH,
                                                                      ".//*[contains(text(), '{}')]".format(text))
 
+    def __check_login_fail(self):
+        return ui.WebDriverWait(self.__driver, 5).until(lambda d: exists_css_selector(d, ".toast-message > .login_error"))
+
     def __handle_assessment_question_pool(self, start_index=0):
         """Handle an assessment question with one or more broken xid links.
         Note that assessment question links actually navigate to question pools and not individual questions."""
@@ -326,20 +329,13 @@ class XIDFixer:
                 )
                 username_input.send_keys(username)
                 self.__driver.find_element(By.ID, "passwordInput").send_keys(password)
-
-                print("Username as entered: {}".format(
-                    self.__driver.find_element(By.ID, "userNameInput").get_attribute("value")))
-                print("Password as entered: {}".format(
-                    self.__driver.find_element(By.ID, "passwordInput").get_attribute("value")))
-
                 self.__driver.find_element(By.ID, "submitButton").click()
 
             except ElementNotInteractableException:
                 return 0, 0, "login_not_interactable"
 
-            login_fail = ui.WebDriverWait(self.__driver, 5).until(lambda d: exists_css_selector(d, ".login_error"))
             print(self.__driver.find_element(By.TAG_NAME, "body").get_attribute("innerHTML"))
-            if login_fail:
+            if self.__check_login_fail():
                 return 0, 0, "login_fail"
 
         self.__go_to_course_link_validator()
